b490d1e9fc5b71dfc176b13a4439be0ed9eecf9c
GlorifiedPig <killingpigs123@gmail.com>
2017-06-09 16:14:41 +0200

    Spawn with weapons

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,7 +4,18 @@ AddCSLuaFile("client/cl_camera.lua")
 AddCSLuaFile("client/cl_hud.lua")
 
 function GM:PlayerSpawn(ply)
+  local Weapons = {"weapon_pistol", "weapon_crowbar"}
+  local Ammo = {"Pistol"}
+
   ply:SetModel( "models/player/Group03/Male_0" .. math.random(1, 9) .. ".mdl" )
+
+  for k, v in pairs( Weapons ) do
+    ply:Give(v)
+  end
+
+  for k, v in pairs( Ammo ) do
+    ply:GiveAmmo( 64, v )
+  end
 end
 
 function GM:GetFallDamage( ply, speed )
