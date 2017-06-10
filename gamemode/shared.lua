154fc9d41f67cb4a2f5a7dba0959b5c71a1fb32f
GlorifiedPig <killingpigs123@gmail.com>
2017-06-10 21:42:50 +0200

    Edited movement system

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -13,3 +13,32 @@ DeriveGamemode( "base" )
 function GM:Initialize()
   self.BaseClass.Initialize( self )
 end
+
+function GM:SetupMove( ply, mv )
+  local velocity = mv:GetVelocity()
+  local p = 0
+  local y = 0
+
+	if mv:KeyDown( IN_JUMP ) then
+		mv:SetButtons( bit.band(mv:GetButtons(), bit.bnot( IN_JUMP ) ) )
+	end
+
+	if mv:KeyDown( IN_FORWARD ) then
+		p = 255
+	end
+
+  if mv:KeyDown( IN_BACK ) then
+		p = -255
+	end
+
+  if mv:KeyDown( IN_MOVELEFT ) then
+		y = 255
+	end
+
+	if mv:KeyDown( IN_MOVERIGHT ) then
+		y = -255
+	end
+
+  velocity = Vector( p, y, 0 )
+  mv:SetVelocity( velocity )
+end
