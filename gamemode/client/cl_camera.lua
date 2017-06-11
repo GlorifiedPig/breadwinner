54ac6710537fe62f0c93954e5e493f3f77707b72
GlorifiedPig <killingpigs123@gmail.com>
2017-06-11 10:55:14 +0200

    Fixed gravity

--- gamemode/client/cl_camera.lua
+++ gamemode/client/cl_camera.lua
@@ -64,3 +64,33 @@ function GM:Think()
     inWorld = true
   end
 end
+
+function GM:Move( ply, mv )
+  local velocity = mv:GetVelocity()
+  local x = 0
+  local y = 0
+	local z = mv:GetVelocity().z
+
+	if mv:KeyDown( IN_JUMP ) then
+		mv:SetButtons( bit.band(mv:GetButtons(), bit.bnot( IN_JUMP ) ) )
+	end
+
+	if mv:KeyDown( IN_FORWARD ) then
+		x = 255
+	end
+
+  if mv:KeyDown( IN_BACK ) then
+		x = -255
+	end
+
+  if mv:KeyDown( IN_MOVELEFT ) then
+		y = 255
+	end
+
+	if mv:KeyDown( IN_MOVERIGHT ) then
+		y = -255
+	end
+
+  velocity = Vector( x, y, z )
+  mv:SetVelocity( velocity )
+end
--- gamemode/server/sv_camera.lua
+++ gamemode/server/sv_camera.lua
@@ -1,18 +1,19 @@
-function GM:SetupMove( ply, mv, cmd )
+function GM:Move( ply, mv )
   local velocity = mv:GetVelocity()
-  local p = 0
+  local x = 0
   local y = 0
+	local z = mv:GetVelocity().z
 
 	if mv:KeyDown( IN_JUMP ) then
 		mv:SetButtons( bit.band(mv:GetButtons(), bit.bnot( IN_JUMP ) ) )
 	end
 
 	if mv:KeyDown( IN_FORWARD ) then
-		p = 255
+		x = 255
 	end
 
   if mv:KeyDown( IN_BACK ) then
-		p = -255
+		x = -255
 	end
 
   if mv:KeyDown( IN_MOVELEFT ) then
@@ -23,6 +24,6 @@ function GM:SetupMove( ply, mv, cmd )
 		y = -255
 	end
 
-  velocity = Vector( p, y, 0 )
+  velocity = Vector( x, y, z )
   mv:SetVelocity( velocity )
 end
